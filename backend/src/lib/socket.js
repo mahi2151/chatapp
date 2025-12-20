05fcf4f51137568ed5a52d81ca10e1346f848023
MAHI YADAV <mahiyadav21nov@gmail.com>
2025-12-20 22:50:49 +0530

    cors update

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -9,7 +9,8 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin: [ENV.CLIENT_URL],
+        origin: ["http://localhost:5173",
+      "https://chatapp-frontend-7kic.onrender.com"],
         credentials: true,
     },
 });
