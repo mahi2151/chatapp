de5dd6fc43e3ee2ff0efac69096d872e91178dc3
MAHI YADAV <mahiyadav21nov@gmail.com>
2025-12-20 22:24:13 +0530

    more changes applied

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,7 +20,7 @@ console.log("CLIENT_URL:", ENV.CLIENT_URL);
 
 app.use(express.json());
 app.use(cors({
-  origin: ["http://localhost:5173", "https://chatapp-o5lz.onrender.com"],
+  origin: ["http://localhost:5173", "https://chatapp-frontend-7kic.onrender.com"],
   credentials: true
 }));
 app.use(cookieParser());
