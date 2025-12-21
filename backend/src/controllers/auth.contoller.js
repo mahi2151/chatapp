971f65e9ae7c90c1d3fa2d60ca3f72373bb2a6d9
MAHI YADAV <mahiyadav21nov@gmail.com>
2025-12-21 17:32:56 +0530

    fix logout cookie settings

--- backend/src/controllers/auth.contoller.js
+++ backend/src/controllers/auth.contoller.js
@@ -80,11 +80,21 @@ export const login = async (req,res)=>{
        };
 
 export const logout =  (_,res)=>{
+    try{
     res.cookie("jwt", "", {
-        maxAge: 0,});
+      maxAge: 0,
+      httpOnly: true,
+      sameSite: process.env.NODE_ENV === "development" ? "lax" : "none",
+      secure: process.env.NODE_ENV !== "development",
+    });
 
-    res.status(200).json({ message: "Logout successful" });
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    console.log("Error in logout controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
 };
+
 
 
 export const updateProfile = async (req, res) => {
